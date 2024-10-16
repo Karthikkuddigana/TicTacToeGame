5aa0cee23c1353e3b12ff231bbd71586fc875892
karthikkuddigana <63944339+Karthikkuddigana@users.noreply.github.com>
2024-10-16 22:40:33 +0530

    Adjusted the logic

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-let myList=[[0,1,1],[0,0,1],[1,0,0]]; 
+let myList=[[1,0,0],[0,0,0],[0,0,0]]; 
 console.log(myList); 
 console.log(myList[1][1])
+const resultList=[]
+let temp3=""; 
+let temp4=""; 
 for(let i=0; i<myList.length; i++){
     let temp=""; 
     let temp2=""; 
+    temp3+=myList[i][i]
+    temp4+=myList[myList.length-1-i][i]
     for(let j=0; j<myList.length; j++){
         temp+=myList[i][j]; 
         temp2+=myList[j][i]; 
     }
     console.log(temp, temp2)
+    resultList.push(temp); 
+    resultList.push(temp2); 
 }
-let temp3=""; 
-let temp4=""; 
-let tempLen=myList.length; 
-let i=0; 
-while(i<tempLen){
-    temp3+=myList[i][i]; 
-    i++
-}
-for(let k=0; k<myList.length; k++){ 
-    temp4+=myList[myList.length-1-k][k]
-    // console.log(myList.length-1-k,k); 
+// let tempLen=myList.length; 
+// let i=0; 
+// while(i<tempLen){
+//     temp3+=myList[i][i]; 
+//     i++
+// }
+// for(let k=0; k<myList.length; k++){ 
+//     temp4+=myList[myList.length-1-k][k]
+//     // console.log(myList.length-1-k,k); 
 
+// }
+resultList.push(temp3, temp4)
+console.log(resultList); 
+for( let i of resultList){
+    if(i=="111"){
+        console.log("1 wins")
+        break; 
+    }
+    else if(i=="000"){
+        console.log("0 wins") ;
+    }
 }
-console.log(temp3); 
-console.log(temp4); 
