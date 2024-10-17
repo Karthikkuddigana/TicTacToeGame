63caa5a8997d01c50e4a8948ecc66744a5cbca1d
karthikkuddigana <63944339+Karthikkuddigana@users.noreply.github.com>
2024-10-17 22:56:19 +0530

    Integrated the logic with the frontend

--- script.js
+++ script.js
@@ -1,41 +1,66 @@
-let myList=[[1,0,0],[0,0,0],[0,0,0]]; 
-console.log(myList); 
-console.log(myList[1][1])
-const resultList=[]
-let temp3=""; 
-let temp4=""; 
-for(let i=0; i<myList.length; i++){
-    let temp=""; 
-    let temp2=""; 
-    temp3+=myList[i][i]
-    temp4+=myList[myList.length-1-i][i]
-    for(let j=0; j<myList.length; j++){
-        temp+=myList[i][j]; 
-        temp2+=myList[j][i]; 
+let myList=[["","",""],["","",""],["","",""]]; 
+const dialog=document.querySelector("#WinnerDialog")
+const winnerDisplay=document.querySelector("dialog span"); 
+const closeButton=document.getElementById("DialogClose"); 
+closeButton.addEventListener("click",()=>{
+    dialog.close(); 
+    location.reload(); 
+} )
+console.log(dialog); 
+function checkArray(currList){
+    const resultList=[]
+    let temp3=""; 
+    let temp4=""; 
+    for(let i=0; i<currList.length; i++){
+        let temp=""; 
+        let temp2=""; 
+        temp3+=currList[i][i]
+        temp4+=currList[currList.length-1-i][i]
+        for(let j=0; j<currList.length; j++){
+            temp+=currList[i][j]; 
+            temp2+=currList[j][i]; 
+        }
+        // console.log(temp, temp2)
+        resultList.push(temp); 
+        resultList.push(temp2); 
     }
-    console.log(temp, temp2)
-    resultList.push(temp); 
-    resultList.push(temp2); 
+    resultList.push(temp3, temp4)
+    console.log(resultList); 
+    for( let i of resultList){
+        if(i=="xxx"){
+            console.log("1 wins")
+            winnerDisplay.textContent="x wins"; 
+            dialog.show(); 
+            return "x wins"; 
+        }
+        else if(i=="ooo"){
+            console.log("0 wins") ;
+            winnerDisplay.textContent="o wins"; 
+            dialog.show(); 
+            return "o wins"
+        }
+    }
 }
-// let tempLen=myList.length; 
-// let i=0; 
-// while(i<tempLen){
-//     temp3+=myList[i][i]; 
-//     i++
-// }
-// for(let k=0; k<myList.length; k++){ 
-//     temp4+=myList[myList.length-1-k][k]
-//     // console.log(myList.length-1-k,k); 
 
-// }
-resultList.push(temp3, temp4)
-console.log(resultList); 
-for( let i of resultList){
-    if(i=="111"){
-        console.log("1 wins")
-        break; 
-    }
-    else if(i=="000"){
-        console.log("0 wins") ;
-    }
+let currentPlayer="x"
+// let clickedItems=new Array(); 
+const buttonList=document.querySelectorAll(".flex-container-inline div"); 
+// console.log(buttonList)
+for(let ele of buttonList){
+    ele.addEventListener('click',(e)=>{
+        // console.log(e.target.innerHTML)
+        if(e.target.innerHTML!="x" && e.target.innerHTML!="o"){
+            let temp={}; 
+            let parentElement=ele.closest("div[rowIndex]"); 
+            temp.row=parentElement.getAttribute("rowIndex"); 
+            temp.col=ele.getAttribute("colIndex"); 
+            e.target.innerHTML=currentPlayer; 
+            myList[temp.row][temp.col]=currentPlayer; 
+            checkArray(myList); 
+            currentPlayer=(currentPlayer=="x")?"o":"x"; 
+            // clickedItems.push(temp); 
+            // console.log(clickedItems); 
+            console.log(myList); 
+        }
+    })
 }
